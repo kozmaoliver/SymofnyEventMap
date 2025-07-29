baa24c3a871d235db2177d0eeb19f374e78bf1a3
oliver.kozma <oliver.kozma@trendency.hu>
2025-07-29 21:02:19 +0200

    fix: namespace fix

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\SymfonyEventMap\DependencyInjection;
+namespace Kozmaoliver\SymfonyEventMap\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
